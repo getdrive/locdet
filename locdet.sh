344c0f6e9f122bb3a618c8ac6bd35248b4fddbe7
GetDrive <getdrive@users.noreply.github.com>
2017-04-07 23:24:44 +0300

    Update locdet.sh

--- locdet.sh
+++ locdet.sh
@@ -20,6 +20,7 @@ echo ""
 curl -i -s -k -X 'POST' -H 'User-Agent: Dalvik/2.1.0 (Linux; U; Android 5.0.1; Nexus 5 Build/LRX22C)' -H 'Content-Type: application/x-www-form-urlencoded' "https://mobile.maps.yandex.net/cellid_location/?clid=1866854&lac=-1&cellid=-1&operatorid=null&countrycode=null&signalstrength=-1&wifinetworks=$mac:-65&app"| grep -E "(HTTP|coordinates)" | sed 's/^[ \t]*//' | sed 's/<//' | sed 's/>//' | sed 's/\///'
 
 else
+echo ""
 echo "!!!Ввод должен быть 12 символов!!!"
 sleep 2
 
@@ -35,6 +36,7 @@ echo ""
 curl -i -s -k -X 'POST' -H 'User-Agent: Dalvik/2.1.0 (Linux; U; Android 5.0.1; Nexus 5 Build/LRX22C)' -H 'Content-Type: application/x-www-form-urlencoded' "https://mobile.maps.yandex.net/cellid_location/?clid=1866854&lac=-1&cellid=-1&operatorid=null&countrycode=null&signalstrength=-1&wifinetworks=$mac:-65&app"| grep -E "(HTTP|coordinates)" | sed 's/^[ \t]*//' | sed 's/<//' | sed 's/>//' | sed 's/\///'
 
 else
+echo ""
 echo "!!!Input must be 12 characters!!!"
 sleep 2
 
