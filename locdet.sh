a2ffa6c2aa0fcb4cead9ae0798d451428fb59ca3
GetDrive <getdrive@users.noreply.github.com>
2017-04-08 12:55:37 +0300

    Update locdet.sh

--- locdet.sh
+++ locdet.sh
@@ -32,7 +32,7 @@ mac=$(echo -n $maca | sed 's/\://g')
 lengh=$(echo -n $mac | wc -c)
 if [ "$lengh" = "12" ]; then 
 echo ""
-curl -i -s -k -X 'POST' -H 'User-Agent: Dalvik/2.1.0 (Linux; U; Android 5.0.1; Nexus 5 Build/LRX22C)' -H 'Content-Type: application/x-www-form-urlencoded' "https://mobile.maps.yandex.net/cellid_location/?clid=1866854&lac=-1&cellid=-1&operatorid=null&countrycode=null&signalstrength=-1&wifinetworks=$mac:-65&app"| grep -E "(HTTP|coordinates)" | sed 's/^[ \t]*//;s/HTTP/HTTP\//;s/coordinates/Coordinates://;s/latitude/Latitude/;s/longitude/Longitude/;s/<//;s/>//; s/\///'
+curl -i -s -k -X 'POST' -H 'User-Agent: Dalvik/2.1.0 (Linux; U; Android 5.0.1; Nexus 5 Build/LRX22C)' -H 'Content-Type: application/x-www-form-urlencoded' "https://mobile.maps.yandex.net/cellid_location/?clid=1866854&lac=-1&cellid=-1&operatorid=null&countrycode=null&signalstrength=-1&wifinetworks=$mac:-65&app"| grep -E "(HTTP|coordinates)" | sed 's/^[ \t]*//;s/HTTP/HTTP\//;s/coordinates/Coordinates:/;s/latitude/Latitude/;s/longitude/Longitude/;s/<//;s/>//; s/\///'
 
 else
 echo ""
